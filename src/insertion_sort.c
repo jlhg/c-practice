70fba92a410ca650c5c5cf9057b957f06b9df673
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-20 13:01:38 +0800

    Remove unused headers

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 
 void insertion_sort(int *data, int len);
--- src/selection_sort.c
+++ src/selection_sort.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 
 void selection_sort(int *data, int len);
