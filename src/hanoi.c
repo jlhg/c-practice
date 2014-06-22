392a663969bbd1ad4e3002b8690e34dd7dadbc8b
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-22 20:58:27 +0800

    hanoi: add disk number

--- src/hanoi.c
+++ src/hanoi.c
@@ -7,10 +7,10 @@ void hanoi(int n, char src, char buffer, char dst);
 void hanoi(int n, char src, char buffer, char dst)
 {
         if (n == 1) {
-                printf("Move disk from %c to %c\n", src, dst);
+                printf("Move disk %d from %c to %c\n", n, src, dst);
         } else {
                 hanoi(n - 1, src, dst, buffer);
-                printf("Move disk from %c to %c\n", src, dst);
+                printf("Move disk %d from %c to %c\n", n, src, dst);
                 hanoi(n - 1, buffer, src, dst);
         }
 }
