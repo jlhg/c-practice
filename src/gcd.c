8cc6523ba2e748ef9d01c9b577c1158490f8a83e
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-22 18:22:12 +0800

    Add gcd

---
+++ src/gcd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int gcd(int i, int j);
+
+
+int gcd(int i, int j)
+{
+        int k = i % j;
+        if (k == 0) {
+                return j;
+        } else {
+                return gcd(j, k);
+        }
+}
+
+
+int main()
+{
+        int i = 10;
+        int j = 12;
+        printf("%d\n", gcd(i, j));
+
+        return 0;
+}
