ea217201eccbedd31e1eab6dde040913cee5e211
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-22 16:28:14 +0800

    fib: make the codes more concise

--- src/fib.c
+++ src/fib.c
@@ -7,13 +7,10 @@ int fib_2(int n);
 
 
 int fib(int n) {
-        if (n == 0) {
-                return 0;
-        } else if (n == 1) {
-                return 1;
-        } else {
-                return fib(n - 1) + fib(n - 2);
-        }
+        if (n == 0 || n == 1)
+                return n;
+
+        return fib(n - 1) + fib(n - 2);
 }
 
 
