12c212f7bc72eb0a8df1268877a27a53999a933e
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-06-23 22:26:17 +0800

    Add palindrome

---
+++ src/palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "stack.h"
+
+int main(void)
+{
+        char string[] = "racecar";
+        stack stack;
+        int i;
+        int half;
+        int length;
+
+        length = strlen(string);
+        half = length / 2;
+        init_stack(&stack);
+
+        for (i = 0; i < half; ++i)
+                push_stack(&stack, string[i]);
+
+        for (i = length - half; i < length; ++i) {
+                if (string[i] != pop_stack(&stack)) {
+                        printf("not a palindrome\n");
+                        return 0;
+                }
+        }
+        printf("palindrome\n");
+
+        return 0;
+}
