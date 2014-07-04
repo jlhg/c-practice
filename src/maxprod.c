9dc18a61dd2d7678a481baa76dad14277962daf6
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-07-04 22:48:48 +0800

    Add the second algorithm for maximum product problem

--- src/maxprod.c
+++ src/maxprod.c
@@ -3,6 +3,7 @@
 
 
 int maxprod(int data[], int len);
+int maxprod_2(int data[], int len);
 
 int maxprod(int data[], int len)
 {
@@ -26,26 +27,58 @@ int maxprod(int data[], int len)
         return max;
 }
 
+/* http://www.ptt.cc/bbs/Soft_Job/M.1404478881.A.E1B.html */
+int maxprod_2(int data[], int len)
+{
+        assert(len >= 2);
+        int i;
+        int max = data[0] * data[1];
+        int current = data[0];
+        for (i = 1; i < len; ++i) {
+                if (data[i] == 0)
+                        current = 1;
+                else
+                        current *= data[i];
+                if (current > max)
+                        max = current;
+        }
+
+        current = data[len - 1];
+        for (i = len - 2; i >= 0; --i) {
+                if (data[i] == 0)
+                        current = 1;
+                else
+                        current *= data[i];
+                if (current > max)
+                        max = current;
+        }
+
+        return max;
+}
+
 
 int main()
 {
         int data[] = {2, -7, 0, 2, 3, 8, -6, 5};
-        printf("Case 1: %d\n", maxprod(data, 8));
+        printf("Case 1: %d\n", maxprod_2(data, 8));
 
         int data_2[] = {-2, 0, 3, 5, -7};
-        printf("Case 2: %d\n", maxprod(data_2, 5));
+        printf("Case 2: %d\n", maxprod_2(data_2, 5));
 
         int data_3[] = {-2, 1, 3};
-        printf("Case 3: %d\n", maxprod(data_3, 3));
+        printf("Case 3: %d\n", maxprod_2(data_3, 3));
 
         int data_4[] = {-2, 2, 2, -2};
-        printf("Case 4: %d\n", maxprod(data_4, 4));
+        printf("Case 4: %d\n", maxprod_2(data_4, 4));
 
         int data_5[] = {2, 4, -3};
-        printf("Case 5: %d\n", maxprod(data_5, 3));
+        printf("Case 5: %d\n", maxprod_2(data_5, 3));
 
         int data_6[] = {2, 5, -1, 2, -1};
-        printf("Case 6: %d\n", maxprod(data_6, 5));
+        printf("Case 6: %d\n", maxprod_2(data_6, 5));
+
+        int data_7[] = {-1, -2, -3, -4, -5, -6, -7};
+        printf("Case 7: %d\n", maxprod_2(data_7, 7));
 
         return 0;
 }
